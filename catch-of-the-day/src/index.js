1e2969c9be0130bd5a73348d1a4cf36e18d86e51
Christine <aqui.christine@gmail.com>
2018-03-04 22:31:47 -0500

    Making first components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from "react";
+import { render } from "react-dom"; // only importing the render method here
+// Components
+import StorePicker from "./components/StorePicker"
+
+
+
+render(<StorePicker />, document.querySelector('#main'));
