4bdb4683ed6d9bc2f898c610722d5ce5089b74a1
Christine <aqui.christine@gmail.com>
2018-03-05 21:51:34 -0500

    added router component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { render } from "react-dom"; // only importing the render method here
 // Components
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+// import StorePicker from './components/StorePicker';
+// import App from './components/App';
+import Router from './components/Router';
 // Style
 import "./css/style.css";
 
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
