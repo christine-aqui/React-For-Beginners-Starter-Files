7a3d0306944bf0c5817782bf7934c9136308c900
Christine <aqui.christine@gmail.com>
2018-03-09 07:19:43 -0500

    more into states

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,13 +5,29 @@ import Order from './Order';
 
 
 class App extends React.Component {
+	state = {
+		fishes: {},
+		order:{}
+};
+addFish = fish =>{
+	//take a copy of the exiting state. you never want to directly mod a state.
+	const fishes = {...this.state.fishes}; /// ... is called an object spread [look it up]
+	//
+	//next add a new fish to the fishes variable
+	fishes[`fish${Date.now()}`] = fish;
+	//
+	//set the new fishes object to state. setState is built in
+	this.setState({ // pass the setState the date you want to update.
+		fishes: fishes });
+};
+
   render(){
     return <div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline='Fresh Seafood Market' />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>;
   }
 }
