35ccf9c76b7546dd901f805f77b9547f1003202c
Christine <aqui.christine@gmail.com>
2018-03-10 10:43:14 -0500

    loading state with a click

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import samplesFishes from '../sample-fishes';
 
 
 class App extends React.Component {
@@ -20,14 +21,21 @@ addFish = fish =>{
 	this.setState({ // pass the setState the date you want to update.
 		fishes: fishes });
 };
+loadSamplesFishes = () => { // this is a customer function, when button is clicked will push data out
+	this.setState({ fishes : samplesFishes});
+	// fishes is the state being updates
+	// updating fishes with samplesFIshes.
+}
 
   render(){
     return <div className="catch-of-the-day">
 				<div className="menu">
-					<Header tagline='Fresh Seafood Market' />
+					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory
+				addFish={this.addFish}
+				loadSamplesFishes={this.loadSamplesFishes} />
 			</div>;
   }
 }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -3,12 +3,12 @@ import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
 	render() {
-		return (
-			<div className="inventory">
+		return <div className="inventory">
 				<h2>Inventory!!</h2>
-				<AddFishForm  addFish={this.props.addFish}/>
-			</div>
-		);
+				<AddFishForm addFish={this.props.addFish} />
+				<button onClick={this.props.loadSamplesFishes}>
+				Load Sample Fishes</button>
+			</div>;
 	}
 }
 
